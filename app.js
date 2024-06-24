e4b6d0539b68d677bb0e7c52d471827725f4631a
priyansh0202-coder <priyanshsoni36@gmail.com>
2024-06-24 13:18:33 +0530

    .

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const cors = require("cors")
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const db = require("./db")
 const bodyParser = require('body-parser');
 // Define routes
-const indexRouter = require('./routes/index');
+const indexRouter = require('./routes/index.js');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Use routes
